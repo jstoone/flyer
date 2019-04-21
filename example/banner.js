7f9d776377f8b033f46ea5db1b0d776f767713a7
jstoone <jstoone@drk.sh>
2019-04-21 03:49:14 +0200

    Add mandatory image of Carsten.

--- example/banner.js
+++ example/banner.js
@@ -1,18 +1,27 @@
-var Banner = function(core) {
+var Banner = function() {
     this.config = {
         width: 930,
         height: 180,
     }
 
     this.loadQueue = [
-        // 'logo.png'
+        'carsten.png',
     ];
 
     this.init = function() {
-        console.log('init()');
+        this.assets.carsten.x = this.assets.carsten.width + 10;
+        this.assets.carsten.y = this.config.height / 2;
+        this.assets.carsten.anchor.set(.5, .5);
+        this.stage.addChild(this.assets.carsten);
     }
 
     this.start = function() {
-        console.log('start()');
+        anime({
+            targets: this.assets.carsten,
+            duration: 2000,
+            x: this.config.width / 2,
+            angle: 360,
+            easing: 'easeInOutBack',
+        });
     }
 }
